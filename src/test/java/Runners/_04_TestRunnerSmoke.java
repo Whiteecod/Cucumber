5376b063c82237e7569d91f8af2448304c828b2d
kubilayculha <kubilay__culha@hotmail.com>
2023-05-04 19:36:38 +0300

    SmokeTest - Jenkins

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "SmokeTest", // hangi senaryolarda bu
+        tags = "@SmokeTest", // hangi senaryolarda bu
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinitions"},
         plugin = {"html:target/site/cucumber-pretty.html"}
